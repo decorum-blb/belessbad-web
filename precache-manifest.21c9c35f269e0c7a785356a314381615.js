ca90666a04d6184e166a149cb4acd7dc95c38ec1
Adam Edwards <adamcharlesedwards@gmail.com>
2019-09-07 21:54:30 -0500

    gh-pages attempt 2

---
+++ precache-manifest.21c9c35f269e0c7a785356a314381615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4f1e18f244460cb676160dcf80e514",
+    "url": "/thatadamedwards/belessbad-web/index.html"
+  },
+  {
+    "revision": "307bd3e6a936900f2662",
+    "url": "/thatadamedwards/belessbad-web/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "5d0dce747330af4cca54",
+    "url": "/thatadamedwards/belessbad-web/static/js/2.d55cc32f.chunk.js"
+  },
+  {
+    "revision": "307bd3e6a936900f2662",
+    "url": "/thatadamedwards/belessbad-web/static/js/main.f523f3c6.chunk.js"
+  },
+  {
+    "revision": "260a645386a35ee1dd6a",
+    "url": "/thatadamedwards/belessbad-web/static/js/runtime~main.c941f6ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/thatadamedwards/belessbad-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
